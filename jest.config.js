103462328d2f2d14a04cea0331f4f9d7230352cd
RomanoBoldrin <107365022+RomanoBoldrin@users.noreply.github.com>
2026-08-22 11:39:14 -0300

    test: add `jest.config.js` file

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  globalSetup: "<rootDir>/jest.globalSetup.cjs",
+
+  moduleNameMapper: {
+    // Maps imports starting with "@/..." to the project's src directory
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
